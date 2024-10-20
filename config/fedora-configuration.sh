6bd3f285d833ac166dfc0d0af3085144dcaf7468
Pavel Hrdina <pavel.hrdina.ml@seznam.cz>
2024-10-20 16:46:29 +0200

    Add fedora-configuration script for new system automation

---
+++ config/fedora-configuration.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This is licensed under the GNU GPL v3.0 license
+# Created by: github.com/pavel-hrdina
+# Description: Will configure Redhat based systems, to have my preferred settings and tooling installed.
+# Usage: bash fedora-configuration.sh
+# Version: 0.0.1
